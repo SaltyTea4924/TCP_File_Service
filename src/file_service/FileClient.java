948d0e19b6b4004dd0f42e70fb7b155da51ff5cb
SaltyTea4924 <tanner.bauserman@bsu.edu>
2023-10-06 13:35:23 -0400

    client side of upload and some test files

--- src/file_service/FileClient.java
+++ src/file_service/FileClient.java
@@ -1,7 +1,10 @@
 package file_service;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
@@ -40,14 +43,55 @@ public class FileClient {
                     break;
                 }
 
-                case "U":
+                case "U": {
+                    System.out.println("Please enter file name");
+                    String fileName = keyboard.nextLine();
+                    File file = new File("client_folder/"+fileName);
+                    if (!file.exists()){
+                        System.out.println("File doesn't exist!");
+                        break;
+                    }
+                    ByteBuffer request = ByteBuffer.allocate(2000);
+                    request.put(command.getBytes());
+                    request.putInt(fileName.length());
+                    request.put(fileName.getBytes());
+                    FileInputStream fs = new FileInputStream(file);
+                    FileChannel fc = fs.getChannel();
 
+                    SocketChannel channel = SocketChannel.open();
+                    channel.connect(new InetSocketAddress(args[0], serverPort));
+                    do {
+                        request.flip();
+                        channel.write(request);
+                        request.clear();
+                    }while (fc.read(request)>=0);
+                    channel.shutdownOutput();
+                    channel.close();
+                    //TODO: recieve the status code from server
                     break;
+                }
+                case "G": {
+                    System.out.println("Please enter file name");
+                    String filename = keyboard.nextLine();
+                    ByteBuffer request = ByteBuffer.wrap((command + filename).getBytes(StandardCharsets.UTF_8));
+                    SocketChannel channel = SocketChannel.open();
+                    channel.connect(new InetSocketAddress(args[0], serverPort));
+                    channel.write(request);
+                    channel.shutdownOutput();
 
-                case "G":
-
+                    ByteBuffer code = ByteBuffer.allocate(2500);
+                    channel.read(code);
+                    channel.close();
+                    code.flip();
+                    byte[] a = new byte[STATUS_CODE_LENGTH];
+                    code.get(a);
+                    File file = new File("client_folder" + new String(a));
+                    if (!file.exists()){
+                        file.createNewFile();
+                    }
+                    System.out.println(new String(a));
                     break;
-
+                }
                 case "R": {
                     System.out.println("Please enter file name");
                     String oldName = keyboard.nextLine();
@@ -61,6 +105,7 @@ public class FileClient {
 
                     ByteBuffer c = ByteBuffer.allocate(1);
                     socket.read(c);
+                    socket.close();
                     c.flip();
                     byte[] b = new byte[STATUS_CODE_LENGTH];
                     c.get(b);
@@ -75,11 +120,14 @@ public class FileClient {
                     chan.write(list);
                     chan.shutdownOutput();
 
-                    ByteBuffer bytes = ByteBuffer.allocate(25000);
+                    ByteBuffer bytes = ByteBuffer.allocate(2500);
                     chan.read(bytes);
+                    chan.close();
                     bytes.flip();
                     byte[] l = new byte[bytes.toString().length()];
+                    bytes.get(l);
                     System.out.println(new String(l));
+                    chan.close();
                     break;
 
                 default:
